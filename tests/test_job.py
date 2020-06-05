6d93397bdd327a71b914935b4927105991b2393d
Rasmus Munk <munk1@live.dk>
2020-06-05 21:33:08 +0200

    Remove debug print

--- tests/test_job.py
+++ tests/test_job.py
@@ -7,7 +7,6 @@ class TestJOB(unittest.TestCase):
     def test_process(self):
         execute_kwargs = dict(command="/bin/echo", args=["Hello World"], capture=True)
         results = process(execute_kwargs=execute_kwargs)
-        print(results)
         expected_results = dict(
             command="/bin/echo Hello World",
             returncode="0",
